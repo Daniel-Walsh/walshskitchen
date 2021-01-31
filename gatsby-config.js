e20b6e6d6ab5fd9480a12e142377b069cb81a9b4
Dan Walsh <danwalsh@Dans-MacBook-Pro-2.local>
2021-01-31 14:31:20 +1100

    PWA updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
         start_url: `/recipes/`,
         background_color: `#f8f9fa`,
         theme_color: `#e83333`,
-        display: `standalone`,
+        display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
         icon_options: {
           // For all the options available,
