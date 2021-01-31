e2479052232592adf409e71e5db50594a016db82
Dan Walsh <danwalsh@Dans-MacBook-Pro-2.local>
2021-01-31 14:01:40 +1100

    Manifest tweaks.

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,11 +34,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Walsh's Kitchen`,
+        short_name: `Walsh's Kitchen`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f8f9fa`,
+        theme_color: `#e83333`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
