aaddb45a3fa0adc1c3e86dda972e7550fc4a82f9
Dan Walsh <danwalsh@Dans-MacBook-Pro.local>
2021-08-12 08:36:15 +1000

    Updated css purge safelist.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "bg-purple-200",
+    "hover:bg-purple-300",
+    "text-purple-700",
+    "hover:text-purple-900",
+    "bg-blue-200",
+    "hover:bg-blue-300",
+    "text-blue-700",
+    "hover:text-blue-900",
+    "bg-green-200",
+    "hover:bg-green-300",
+    "text-green-700",
+    "hover:text-green-900",
+    "bg-gray-200",
+    "hover:bg-gray-300",
+    "text-gray-400",
+    "hover:text-primary",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
